64d5a5ead8429cf159a39e94e8e291f580c81047
Cass-Kennedy <alkennedy228@gmail.com>
2025-03-24 19:14:18 -0400

    Mod doesn't work on non-integers :(

--- CassKennedyCh4Project/CassKennedyCh4Project.cpp
+++ CassKennedyCh4Project/CassKennedyCh4Project.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     int weight, distance;
+    float lowWeightFare, lowMidWeightFare, midWeightFare, highWeightFare;
+    lowWeightFare = 1.1;
+    lowMidWeightFare = 2.2;
+    midWeightFare = 3.7;
+    highWeightFare = 4.8;
 
     cout << "Enter the weight of your package in kilograms: ";
     cin >> weight;
@@ -13,16 +18,16 @@ int main()
         cout << "Please enter a valid weight and distance";
     }
     else if (weight < 2 ) {
-        cout << "The price of shipping is $" << ;
+        cout << "The price of shipping is $" << ((distance / 500.0) + (1 - ((distance / 500.0)) % 1)) *lowWeightFare;
     }
     else if (weight >= 2 && weight < 6) {
-        cout << "The price of shipping is $" << ;
+        cout << "The price of shipping is $" << ((distance / 500.0) + (1 - ((distance / 500.0)) % 1)) * lowMidWeightFare;
     }
     else if (weight >= 6 && weight < 10) {
-        cout << "The price of shipping is $" << ;
+        cout << "The price of shipping is $" << ((distance / 500.0) + (1 - ((distance / 500.0)) % 1)) * midWeightFare;
     }
     else if (weight >= 10 && weight < 20) {
-        cout << "The price of shipping is $" << ;
+        cout << "The price of shipping is $" << ((distance / 500.0) + (1 - ((distance / 500.0)) % 1)) * highWeightFare;
     }
     return 0;
 }
