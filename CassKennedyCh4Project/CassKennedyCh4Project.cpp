dd660ec4f3784fe5ffdd0e8efd4c9112d06b9880
Cass-Kennedy <alkennedy228@gmail.com>
2025-03-24 18:41:07 -0400

    Wrote a program to give the user the ability to calculate multiple shapes.

--- CassKennedyCh4Project/CassKennedyCh4Project.cpp
+++ CassKennedyCh4Project/CassKennedyCh4Project.cpp
@@ -3,24 +3,38 @@
 using namespace std;
 int main()
 {
-    int month, year;
+    int choice, radius, length, width, base, height;
+    float pi = 3.14159;
 
-    cout << "Enter the month then the year as numbers with a space in between: ";
-    cin >> month;
-    cin >> year;
-    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-        cout << "The month has 31 days";
-    }
-    else if (month != 2) {
-        cout << "The month has 30 days";
-    }
-    else {
-        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            cout << "The month has 29 days.";
-        }
-        else {
-            cout << "The month has 28 days.";
-        }
-    }
+    cout << "1. Calculate the Area of a Circle\n";
+    cout << "2. Calculate the Area of a Rectangle\n";
+    cout << "3. Calculate the area of a Triangle\n";
+    cout << "4. Quit\n";
+    cout << "Enter the number that corresponds to the type of shape you would like to find the area of.\n";
+    cin >> choice;
+        switch (choice) {
+        case 1:
+            cout << "Enter the radius of the circle ";
+            cin >> radius;
+            cout << radius * radius * pi << " u^2";
+            break;
+        case 2:
+            cout << "Enter the rectangle's side lengths ";
+            cin >> length;
+            cin >> width;
+            cout << length * width << " u^2";
+            break;
+        case 3:
+            cout << "Enter the base and height of the triangle ";
+            cin >> base;
+            cin >> height;
+            cout << 0.5 * base * height << " u^2";
+            break;
+        case 4:
+            cout << "\nGoodbye";
+            break;
+        default:
+            cout << "Please enter a valid option\n";
+}
     return 0;
 }
